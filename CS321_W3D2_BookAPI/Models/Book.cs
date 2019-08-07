ff03b9ab826cd51fac99dd5af0f94ed937bbaa59
ck1962 <ckeslin@gmail.com>
2019-08-07 16:07:49 -0500

    updated BookService

--- CS321_W3D2_BookAPI/Models/Book.cs
+++ CS321_W3D2_BookAPI/Models/Book.cs
@@ -6,8 +6,11 @@ namespace CS321_W3D2_BookAPI.Models
     public class Book
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public int AuthorId { get; set; }
+        [Required]
         public Author Author { get; set; }
     }
 }
